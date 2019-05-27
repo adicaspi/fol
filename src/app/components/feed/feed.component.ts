8cfa5acce8f841239333bd404e58ba3d60e90691
Adi Caspi <adicaspi88@gmail.com>
2019-05-27 16:35:00 +0300

    feed profile and product  pages mobile design

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TimelinePost } from '../../models/TimelinePost';
 import { PostService } from '../../services/post.service';
+import { ProductPageComponent } from '../product-page/product-page.component';
+import { DialogService } from '../../services/dialog.service';
 
 @Component({
   selector: 'app-feed',
@@ -10,7 +12,11 @@ import { PostService } from '../../services/post.service';
 export class FeedComponent implements OnInit {
   @Input('post') timelinePost: TimelinePost;
 
-  constructor(private postService: PostService) {}
+  constructor(private dialogService: DialogService) {}
 
   ngOnInit() {}
+
+  openDialog(): void {
+    this.dialogService.openDialog(ProductPageComponent, this.timelinePost);
+  }
 }
