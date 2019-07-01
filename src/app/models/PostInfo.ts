56f87517b9e516d8982f292fd61cc35fc66f05ea
Adi Caspi <adicaspi88@gmail.com>
2019-07-01 18:43:02 +0300

    Add new model to work with post-info method

---
+++ src/app/models/PostInfo.ts
@@ -0,0 +1,11 @@
+export class PostInfo {
+  id: number;
+  userId: number;
+  imageAddr: string;
+  link: string;
+  description: string;
+  price: string;
+  website: string;
+  designer: string;
+  productId: string;
+}
