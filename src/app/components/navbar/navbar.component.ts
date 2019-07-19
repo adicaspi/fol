495426b395903ab7a2285eb014a3fcf3af1723a9
Adi Caspi <adicaspi88@gmail.com>
2019-07-19 08:55:56 +0300

    Fixed bottom navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { UserService } from '../../services/user.service';
-
-import { Routes, Router } from '@angular/router';
+import { Routes, Router, ActivatedRoute } from '@angular/router';
 import { ViewProfileComponent } from '../view-profile/view-profile.component';
 import { RegisterComponent } from '../register/register.component';
 import { SettingsComponent } from '../settings/settings.component';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-navbar',
@@ -14,19 +14,38 @@ import { SettingsComponent } from '../settings/settings.component';
 export class NavbarComponent implements OnInit {
   enabled: boolean = false;
   loggedin = false;
-  feed: boolean = true;
+  feed: boolean = false;
   explore: boolean = false;
   profile: boolean = false;
+  mobile: boolean = false;
   routes: Routes = [
     { path: 'profile/:id', component: ViewProfileComponent },
     { path: '', component: RegisterComponent },
     { path: 'settings/:id', component: SettingsComponent }
   ];
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private deviceService: DeviceDetectorService
+  ) {}
 
   ngOnInit() {
+    if (this.router.url.includes('profile')) {
+      this.profile = true;
+    }
+    if (this.router.url.includes('feed')) {
+      this.feed = true;
+    }
+    if (this.router.url.includes('explore')) {
+      this.explore = true;
+    }
     if (this.userService.userId) {
       this.loggedin = true;
+    }
+
+    if (this.deviceService.isMobile() || this.deviceService.isTablet()) {
+      this.mobile = true;
     }
   }
 
@@ -38,28 +57,20 @@ export class NavbarComponent implements OnInit {
   }
 
   settingsPage() {
-    if (this.loggedin) {
-      this.router.navigate(['settings', this.userService.userId]);
-    } else {
-      this.router.navigate(['']);
-    }
+    this.router.navigate(['settings', this.userService.userId]);
   }
 
   feedPage() {
-    if (this.loggedin) {
-      this.profile = false;
-      this.feed = true;
-      this.explore = false;
-      this.router.navigate(['feed', this.userService.userId]);
-    } else {
-      this.router.navigate(['']);
-    }
+    this.profile = false;
+    this.feed = true;
+    this.explore = false;
+    this.router.navigate(['feed', this.userService.userId]);
   }
 
   explorePage() {
     this.profile = false;
     this.feed = false;
     this.explore = true;
-    this.router.navigate(['/explore']);
+    this.router.navigate(['/explore', this.userService.userId]);
   }
 }
