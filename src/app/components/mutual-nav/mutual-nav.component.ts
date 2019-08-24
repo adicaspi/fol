054e0d03ecb1359fd3910b47c802d4e35a80a902
Adi Caspi <adicaspi88@gmail.com>
2019-08-24 18:55:02 +0300

    Change navbar design and logic

--- src/app/components/mutual-nav/mutual-nav.component.ts
+++ src/app/components/mutual-nav/mutual-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import * as $ from 'jquery';
 
 @Component({
@@ -10,13 +11,16 @@ export class MutualNavComponent implements OnInit {
   //mainList = ['Categories', 'Designers', 'Stores', 'Price'];
   @Output()
   change: EventEmitter<string> = new EventEmitter<string>();
+  categoryForm: FormGroup;
+  productForm: FormGroup;
   class = 'filtered';
   menu_class = 'popup';
   aria_expanded = 'false';
   mainList = {};
   keys = ['Category', 'Product type', 'Designer', 'Store', 'Price'];
-  openDropDownKey: any;
-  prevOpenKey: any;
+  prevOpenID: any;
+  currentOpenKey: any = null;
+  prevOpenKey: any = null;
   Categories = ['Clothings', 'Shoes', 'Bags', 'Accesories'];
   ProductType = [
     'All clothings',
@@ -28,9 +32,9 @@ export class MutualNavComponent implements OnInit {
   ];
   Designers = ['All Designers', 'Gucci', 'Prada', 'D&G'];
   Stores = ['All Stores', 'ASOS', 'ZARA', 'Adika'];
-  Price = ['All Prices', '<1000', '1000-5000', '<5000'];
+  Price = ['All Prices', 'High to Low', 'Low to High'];
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.mainList['Category'] = this.Categories;
@@ -38,28 +42,69 @@ export class MutualNavComponent implements OnInit {
     this.mainList['Designer'] = this.Designers;
     this.mainList['Store'] = this.Stores;
     this.mainList['Price'] = this.Price;
-  }
-
-  changeMenu(key, i) {
-    this.change.emit(this.class);
-    let dropDownID = $('#_' + i);
-
-    if (this.openDropDownKey) {
-      this.prevOpenKey.css({
-        display: 'none'
-      });
-      if (this.openDropDownKey == key) {
-        this.openDropDownKey = null;
-        return;
-      }
-    }
-    this.prevOpenKey = dropDownID;
-    this.openDropDownKey = key;
-    dropDownID.css({
-      display: 'block'
+    this.categoryForm = this.formBuilder.group({
+      Clothings: [''],
+      Shoes: [''],
+      Bags: [''],
+      Accesories: ['']
+    });
+    this.productForm = this.formBuilder.group({
+      Clothings: [''],
+      Shoes: [''],
+      Bags: [''],
+      Accesories: ['']
     });
   }
 
+  changeMenu(key, i) {
+    //this.change.emit(this.class);
+    this.currentOpenKey = key;
+    let currentDropDownID = $('#_' + key);
+    let btn = $('.submit-btn');
+    let currentDropBtn = $('.dropbtn.' + key);
+
+    //checking if this isn't the first time user opens dropbtn, if it's not then display prev key as none;
+    if (this.prevOpenKey) {
+      let prevDropBtn = $('.dropbtn.' + this.prevOpenKey);
+      prevDropBtn.css({
+        color: 'rgb(140, 140, 140)'
+      });
+      this.prevOpenID.css({
+        display: 'none'
+      });
+
+      //if user opened and closes same dropdown then just clear all fields and return
+
+      if (this.prevOpenKey == key) {
+        this.currentOpenKey = null;
+        this.prevOpenKey = null;
+        btn.css({
+          display: 'none'
+        });
+        return;
+
+        //else user openes a new dropdown which is not the same dropdown currently opens
+      } else {
+        this.prevOpenKey = this.currentOpenKey;
+        this.prevOpenID = currentDropDownID;
+      }
+    }
+    // if this is the very first time the user filters the menu
+    else {
+      this.prevOpenKey = key;
+      this.prevOpenID = currentDropDownID;
+    }
+    //anyway do this when user clicks the menu
+    currentDropDownID.css({
+      display: 'block'
+    });
+    btn.css({
+      display: 'initial'
+    });
+    currentDropBtn.css({
+      color: 'black'
+    });
+  }
   getKeys() {
     return this.keys;
   }
--- src/app/components/view-profile/view-profile.component.ts
+++ src/app/components/view-profile/view-profile.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { DeviceDetectorService } from '../../../../node_modules/ngx-device-detector';
+import { MutualNavComponent } from '../mutual-nav/mutual-nav.component';
 
 @Component({
   selector: 'app-view-profile',
   templateUrl: './view-profile.component.html',
-  styleUrls: ['./view-profile.component.css']
+  styleUrls: ['./view-profile.component.css'],
+  host: {
+    '(document:click)': 'onClick($event)'
+  }
 })
 export class ViewProfileComponent implements OnInit {
+  @ViewChild(MutualNavComponent, { static: false })
+  mutualNav: MutualNavComponent;
   desktop: Boolean;
   classToApply: string = 'center';
   isHandset$: Observable<boolean> = this.breakpointObserver
@@ -18,7 +24,8 @@ export class ViewProfileComponent implements OnInit {
 
   constructor(
     private breakpointObserver: BreakpointObserver,
-    private deviceService: DeviceDetectorService
+    private deviceService: DeviceDetectorService,
+    private _eref: ElementRef
   ) {}
 
   ngOnInit() {
@@ -27,7 +34,19 @@ export class ViewProfileComponent implements OnInit {
     }
   }
 
-  countChange(event) {
-    this.classToApply = event;
-  }
+  // countChange(event) {
+  //   this.classToApply = event;
+  // }
+
+  // onClick(event) {
+  //   if (this._eref.nativeElement.contains(event.target))
+  //     console.log('in click event');
+  //   if (this.mutualNav.openDropDownKey) {
+  // or some similar check
+
+  // this.mutualNav.prevOpenKey.css({
+  //   display: 'none'
+  // });
+  //}
+  //  }
 }
