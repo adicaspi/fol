f3586fa6fc3434b8bc5736683b7c2198660ac654
tamiryup <tamirishaysh@gmail.com>
2019-11-07 10:28:48 +0200

    Change explore gutter to 18px

--- src/app/components/explore-feed/explore-feed.component.ts
+++ src/app/components/explore-feed/explore-feed.component.ts
@@ -27,7 +27,7 @@ export class ExploreFeedComponent implements OnInit {
   public masonryOptions: NgxMasonryOptions = {
     transitionDuration: '0',
     horizontalOrder: true,
-    gutter: 20,
+    gutter: 18,
     fitWidth: true
   };
   constructor(
