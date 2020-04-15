b38df606a6ca2918ce507bd472a7f288b4dd9483
Adi Caspi <adicaspi88@gmail.com>
2020-04-15 20:46:33 +0300

    Seperate user-profile-info to mobile and desktop

--- src/app/components/user-profile-info-desktop/user-profile-info-desktop.component.ts
+++ src/app/components/user-profile-info-desktop/user-profile-info-desktop.component.ts
@@ -5,14 +5,43 @@ import { UserService } from '../../services/user.service';
 import { ActivatedRoute, Router } from '../../../../node_modules/@angular/router';
 import { DialogService } from '../../services/dialog.service';
 import { ConfigService } from '../../services/config.service';
+import { Observable, Subject, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { GenerateFollowListComponent } from '../generate-follow-list/generate-follow-list.component';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-user-profile-info-desktop',
   templateUrl: './user-profile-info-desktop.component.html',
   styleUrls: ['./user-profile-info-desktop.component.scss']
 })
-export class UserProfileInfoDesktopComponent extends UserProfileInfoComponent {
+export class UserProfileInfoDesktopComponent implements OnInit {
   @Input() user;
+  public currMasterId: number;
+  public slaveId: number;
+  public follows: boolean;
+  //public user: Observable<User>;
+  public userId: number;
+  public userProfileImageSrc: string;
+  public src: any;
+  public following: Observable<number>;
+  public followers: Observable<number>;
+  public numberOfPosts: Observable<number>;
+  public userLoaded: Promise<boolean>;
+  public flag: number = 1;
+  public clicked: boolean = false;
+  public onDestroy: Subject<void> = new Subject<void>();
+  public desktop: boolean = false;
+  public userProfile: boolean = false;
+  public subscription: Subscription;
+  public msgSubscription: Subscription;
+  public anyErrors: boolean;
+  public finished: boolean;
+  public followingDialogRef: MatDialogRef<{}, any>;
+  public WindowSizeSubscription: Subscription;
+  public previousUrl: string = "";
+  public currentUrl: string = "";
+
   constructor(public userService: UserService,
     public activatedRoute: ActivatedRoute,
     public dialogService: DialogService,
@@ -20,7 +49,117 @@ export class UserProfileInfoDesktopComponent extends UserProfileInfoComponent {
     public configService: ConfigService,
     public location: LocationService
   ) {
-    super(userService, activatedRoute, dialogService, router, configService, location);
+
   }
+  ngOnInit() {
+    //console.log("user", this.user);
+    this.userId = this.userService.userId;
+    const routeParams = this.activatedRoute.snapshot.params;
+    this.currMasterId = parseInt(routeParams.id);
+    if (this.userId == this.currMasterId) {
+      this.userProfile = true;
+    }
+
+    // this.WindowSizeSubscription = this.configService.windowSizeChanged
+    //   .subscribe(
+    //     value => {
+    //       if (value.width >= 600) {
+    //         this.desktop = true;
+    //       }
+
+    //       if (value.width <= 600) {
+    //         this.desktop = false;
+    //       }
+    //     });
+
+
+
+
+    this.updateUser(this.currMasterId);
+    this.userService.checkIsFollowing(this.currMasterId).pipe(takeUntil(this.onDestroy)).subscribe(res => {
+      this.follows = res;
+    })
+    this.getNumFollowers();
+    this.getNumFollowing();
+    this.getNumPosts();
+  }
+
+  getNumFollowing() {
+    this.following = this.userService.getNumberOfFollowing(this.currMasterId);
+  }
+
+  getNumFollowers() {
+    this.followers = this.userService.getNumberOfFollowers(this.currMasterId);
+  }
+
+  getNumPosts() {
+    this.numberOfPosts = this.userService.getNumberOfPosts(this.currMasterId);
+  }
+
+  updateUser(id) {
+    // this.user = this.userService.getUserDetails(id);
+    //this.user = this.userService.getUserProfileInfo(id);
+  }
+
+  follow() {
+    //if user is already following then unfollow
+    if (this.follows) {
+      this.userService.unFollow(this.currMasterId);
+      this.follows = false;
+    } else {
+      this.userService.follow(this.currMasterId);
+      this.follows = true;
+    }
+  }
+
+  modalFlag(flag: number) {
+    this.flag = flag;
+    this.clicked = true;
+  }
+
+  openDialog(flag) {
+    var title;
+    if (flag) {
+      title = 'Following';
+    } else {
+      title = 'Followers';
+    }
+    this.dialogService.followingDialogDataObject.flag = flag;
+    this.dialogService.followingDialogDataObject.userId = this.currMasterId;
+    this.dialogService.followingDialogDataObject.title = title;
+    var componentName = 'followersList';
+    this.dialogService.openModalWindow(
+      GenerateFollowListComponent,
+      componentName
+    );
+    this.dialogService.desktop = true;
+    this.followingDialogRef = this.dialogService.followingDialogRef;
+    this.followingDialogRef.afterClosed().pipe(takeUntil(this.onDestroy)).subscribe(result => {
+      if (result) {
+        this.getNumFollowing();
+      }
+    });
+
+
+  }
+
+
+  settingsPage() {
+    this.router.navigate(['settings', this.userService.userId]);
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['landing']);
+  }
+
+  goBackPage() {
+    this.location.goBack();
+  }
+
+  ngOnDestroy(): void {
+    this.WindowSizeSubscription.unsubscribe();
+  }
+
 
 }
--- src/app/components/user-profile-info-mobile/user-profile-info-mobile.component.ts
+++ src/app/components/user-profile-info-mobile/user-profile-info-mobile.component.ts
@@ -1,18 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { LocationService } from '../../services/location.service';
 import { UserService } from '../../services/user.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DialogService } from '../../services/dialog.service';
 import { ConfigService } from '../../services/config.service';
 import { UserProfileInfoComponent } from '../user-profile-info/user-profile-info.component';
+import { Observable, Subject, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-profile-info-mobile',
   templateUrl: './user-profile-info-mobile.component.html',
   styleUrls: ['./user-profile-info-mobile.component.scss']
 })
-export class UserProfileInfoMobileComponent extends UserProfileInfoComponent {
+export class UserProfileInfoMobileComponent implements OnInit {
   @Input() user;
+  public currMasterId: number;
+  public slaveId: number;
+  public follows: boolean;
+  //public user: Observable<User>;
+  public userId: number;
+  public userProfileImageSrc: string;
+  public src: any;
+  public following: Observable<number>;
+  public followers: Observable<number>;
+  public numberOfPosts: Observable<number>;
+  public userLoaded: Promise<boolean>;
+  public flag: number = 1;
+  public clicked: boolean = false;
+  public onDestroy: Subject<void> = new Subject<void>();
+  public desktop: boolean = false;
+  public userProfile: boolean = false;
+  public subscription: Subscription;
+  public msgSubscription: Subscription;
+  public anyErrors: boolean;
+  public finished: boolean;
+  //public followingDialogRef: MatDialogRef<{}, any>;
+  public WindowSizeSubscription: Subscription;
+  public previousUrl: string = "";
+  public currentUrl: string = "";
+
   constructor(public userService: UserService,
     public activatedRoute: ActivatedRoute,
     public dialogService: DialogService,
@@ -20,7 +47,107 @@ export class UserProfileInfoMobileComponent extends UserProfileInfoComponent {
     public configService: ConfigService,
     public location: LocationService
   ) {
-    super(userService, activatedRoute, dialogService, router, configService, location);
+
   }
+
+  ngOnInit() {
+    //console.log("user", this.user);
+    this.userId = this.userService.userId;
+    const routeParams = this.activatedRoute.snapshot.params;
+    this.currMasterId = parseInt(routeParams.id);
+    if (this.userId == this.currMasterId) {
+      this.userProfile = true;
+    }
+
+    this.WindowSizeSubscription = this.configService.windowSizeChanged
+      .subscribe(
+        value => {
+          if (value.width >= 600) {
+            this.desktop = true;
+          }
+
+          if (value.width <= 600) {
+            this.desktop = false;
+          }
+        });
+
+
+
+
+    this.updateUser(this.currMasterId);
+    this.userService.checkIsFollowing(this.currMasterId).pipe(takeUntil(this.onDestroy)).subscribe(res => {
+      this.follows = res;
+    })
+    this.getNumFollowers();
+    this.getNumFollowing();
+    this.getNumPosts();
+  }
+
+  getNumFollowing() {
+    this.following = this.userService.getNumberOfFollowing(this.currMasterId);
+  }
+
+  getNumFollowers() {
+    this.followers = this.userService.getNumberOfFollowers(this.currMasterId);
+  }
+
+  getNumPosts() {
+    this.numberOfPosts = this.userService.getNumberOfPosts(this.currMasterId);
+  }
+
+  updateUser(id) {
+    // this.user = this.userService.getUserDetails(id);
+    //this.user = this.userService.getUserProfileInfo(id);
+  }
+
+  follow() {
+    //if user is already following then unfollow
+    if (this.follows) {
+      this.userService.unFollow(this.currMasterId);
+      this.follows = false;
+    } else {
+      this.userService.follow(this.currMasterId);
+      this.follows = true;
+    }
+  }
+
+  modalFlag(flag: number) {
+    this.flag = flag;
+    this.clicked = true;
+  }
+
+  openDialog(flag) {
+    var title;
+    if (flag) {
+      title = 'Following';
+    } else {
+      title = 'Followers';
+    }
+    this.dialogService.followingDialogDataObject.flag = flag;
+    this.dialogService.followingDialogDataObject.userId = this.currMasterId;
+    this.dialogService.followingDialogDataObject.title = title;
+    this.dialogService.desktop = false;
+    this.router.navigate(['following']);
+
+  }
+
+
+  settingsPage() {
+    this.router.navigate(['settings', this.userService.userId]);
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['landing']);
+  }
+
+  goBackPage() {
+    this.location.goBack();
+  }
+
+  ngOnDestroy(): void {
+    this.WindowSizeSubscription.unsubscribe();
+  }
+
 
 }
