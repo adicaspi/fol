183d4d82f44b619427a140115f200f63112eef9b
Adi Caspi <adicaspi88@gmail.com>
2020-04-20 16:11:58 +0300

    Fix dev url

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   environmentName: 'dev',
-  BASE_API_URL: 'https://www.devenv.followear.com/api'
+  BASE_API_URL: 'https://devenv.followear.com/api'
 };
