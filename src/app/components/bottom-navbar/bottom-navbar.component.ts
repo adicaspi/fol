a02d82c58c958d8b13c1efd67f549d63be41144c
Adi Caspi <adicaspi88@gmail.com>
2020-07-21 16:56:41 +0300

    Highlight profile icon when profile page is active

--- src/app/components/bottom-navbar/bottom-navbar.component.ts
+++ src/app/components/bottom-navbar/bottom-navbar.component.ts
@@ -51,37 +51,37 @@ export class BottomNavbarComponent implements OnInit {
   }
 
   ngOnInit() {
+
     this.activatedRoute.url
       .pipe(takeUntil(this.onDestroy))
       .subscribe(params => {
-        if (params.length > 1) {
-          switch (params[0].path) {
-            case "feed":
-              this.feed = true;
-              this.explore = false;
+        switch (params[0].path) {
+          case "feed":
+            this.feed = true;
+            this.explore = false;
+            this.profile = false;
+            break;
+          case "profile":
+            if (params.length > 1) {
               this.profile = false;
-              break;
-            case "profile":
-              if (params[1].path == this.userService.getCurrentUser()) {
-                this.profile = true;
-              }
-              else {
-                this.profile = false;
-              }
-              this.feed = false;
-              this.explore = false;
-              break;
-            case "explore":
-              this.feed = false;
-              this.explore = true;
-              this.profile = false;
-              break;
-            default:
-              this.feed = false;
-              this.explore = false;
-              this.profile = false;
-          }
+            }
+            else {
+              this.profile = true;
+            }
+            this.feed = false;
+            this.explore = false;
+            break;
+          case "explore":
+            this.feed = false;
+            this.explore = true;
+            this.profile = false;
+            break;
+          default:
+            this.feed = false;
+            this.explore = false;
+            this.profile = false;
         }
+
       });
     this.prevScrollpos = window.pageYOffset;
   }
