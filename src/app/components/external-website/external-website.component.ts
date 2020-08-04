c7a260c52f0b20d3cd1e01cce56303f4e3c645ec
Adi Caspi <adicaspi88@gmail.com>
2020-08-04 09:37:38 +0300

    Add app banner to product page mobile

--- src/app/components/external-website/external-website.component.ts
+++ src/app/components/external-website/external-website.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+(window as any).jQuery = $;
 
 @Component({
   selector: 'app-external-website',
@@ -10,6 +12,34 @@ export class ExternalWebsiteComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.initCarousel(this);
+  }
+
+  initCarousel(that) {
+
+    $('#recipeCarousel').carousel({
+      interval: 10000
+    })
+
+    $('.carousel .carousel-item').each(function () {
+      var minPerSlide = 3;
+      var next = $(this).next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+
+      for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+          next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+      }
+    });
+
+
   }
 
 }
