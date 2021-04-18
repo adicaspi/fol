a32f358b3042b9c12806158138ba119dc532ee0e
Adi Caspi <adicaspi88@gmail.com>
2021-04-18 21:12:18 +0300

    Change userID and posts in external website

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,10 +3,10 @@ export const environment = {
   BASE_API_URL: 'https://www.followear.com/api',
   loginWithFbUrl: 'https://prodauth.followear.com/oauth2/authorize?identity_provider=Facebook&redirect_uri=https://www.followear.com&response_type=CODE&client_id=58mgmbee18dn4goc7rd5427cdr&scope=email%20openid%20aws.cognito.signin.user.admin%20profile',
   bloggersPosts: [{ userId: 155, postId: 1598 },
-  { userId: 300, postId: 1652 },
+  { userId: 320, postId: 1839 },
   { userId: 303, postId: 1641 },
   { userId: 104, postId: 553 },
-  { userId: 303, postId: 1637 }]
+  { userId: 320, postId: 1838 }]
 };
 
 
